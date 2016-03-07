fd10e6c3c904e7d6cf2ad2b2c9dc597995f2d91d
Joao Sone <sousa.joao.neto@gmail.com>
2016-03-07 19:49:18 -0300

    mapeamento-inline
    Mudança no mapeamento do aluno(AlunoServlet)
    Retirado do web.xml e inserido no servlet

--- src/br/edu/fjn/servlets/AlunoServlet.java
+++ src/br/edu/fjn/servlets/AlunoServlet.java
@@ -3,6 +3,7 @@ package br.edu.fjn.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import br.edu.fjn.model.Aluno;
  * Servlet implementation class AlunoServlet
  */
 
+@WebServlet("/aluno")
 public class AlunoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
